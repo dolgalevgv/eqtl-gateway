07397cd50973678cb9baa3cf88bd7638ef2335d8
dolgalev <dolgalev@uni-muenster.de>
2025-01-23 10:13:58 +0100

    Increase lambda to 0.8

--- scripts/albert2018/04_build_ggm.R
+++ scripts/albert2018/04_build_ggm.R
@@ -11,8 +11,8 @@ RcppParallel::setThreadOptions(numThreads = 24)
 
 expr_irn_rc_train <- readr::read_tsv("../../data/albert2018/interim/albert2018_expression_logtpm_irn_regcov_train.tsv")
 
-ggm <- FastGGM::FastGGM_Parallel(as.matrix(expr_irn_rc_train), lambda = 0.5)
+ggm <- FastGGM::FastGGM_Parallel(as.matrix(expr_irn_rc_train), lambda = 0.8)
 
 ggm[["gene_id"]] <- colnames(expr_irn_rc_train)
 
-saveRDS(ggm, "../../results/albert2018/expr_ggm.rds")
+saveRDS(ggm, "../../results/albert2018/expr_ggm_lambda08.rds")
